f2704af4afdc02b3f7f3f442f43a70bf573fa286
Anish Kanna <sautronik@gmail.com>
2022-03-31 15:03:12 +0530

    done

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,52 @@ import '../styles/global.sass'
 
 
 import Layout from '../components/Layout'
+import { motion, AnimatePresence } from "framer-motion"
 
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps , router }) {
 
 
   return (
+<Layout>
+<AnimatePresence >
 
-  <Layout>
+  <motion.div
+  key={router.route} initial="pageInitial" animate="pageAnimate" exit="exit"
+    variants={{
+      pageInitial: {
+        opacity: 0, x: 100,
+        transition: {
+          staggerChildren: 1,
+          delay: .2,
+          
+        }
+      },
+      pageAnimate: {
+        opacity: 1, x: 0,
+        transition: {
+          staggerChildren: 1,
+          
+        }
+       
+      },
+      exit: {
+        opacity: 0, x: -300,
+        transition: {
+          staggerChildren: 1 ,
+          duration: .2,
+          
+        }
+      },
+
+    }   }
+
+
+  >
 
  <Component {...pageProps} />
 
+  </motion.div></AnimatePresence>
   </Layout>
 
 )
