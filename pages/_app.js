6d7859c08ae618d21bc34adb0f6ffc71406cce1a
Anish Kanna <sautronik@gmail.com>
2022-04-09 10:37:44 +0530

    added smooth transition

--- pages/_app.js
+++ pages/_app.js
@@ -45,7 +45,7 @@ function MyApp({ Component, pageProps , router }) {
         <title>My page title</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preconnect" href="https://fonts.googleapis.com"></link> 
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
         <link href="https://fonts.googleapis.com/css2?family=Bellefair&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@100;300&display=swap" rel="stylesheet"></link>
       </Head>
